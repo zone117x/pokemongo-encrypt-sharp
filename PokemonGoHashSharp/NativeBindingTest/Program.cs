5e32afaee17c46b6409678374767aa8d4a3f4f07
Matthew Little <zone117x@gmail.com>
2016-08-07 10:38:19 -0600

    Progress on managed implementation, created native p/invoke wrapper, created native vs managed test

---
+++ PokemonGoHashSharp/NativeBindingTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeBindingTest
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var sampleOutputNative = TestNative();
+         var sampleOutputManaged = TestManaged();
+
+         for (var i = 0; i < Math.Min(sampleOutputNative.Length, sampleOutputManaged.Length); i++)
+         {
+            if (sampleOutputNative[i] != sampleOutputManaged[i])
+            {
+               throw new Exception($"Output buffer difference at index {i}: native is {sampleOutputNative[i]}, managed is {sampleOutputManaged[i]}");
+            }
+         }
+
+         if (sampleOutputManaged.Length != sampleOutputNative.Length)
+         {
+            throw new Exception($"Output length differences: native is {sampleOutputNative.Length}, managed is {sampleOutputManaged.Length}");
+         }
+      }
+
+      static byte[] TestNative()
+      {
+         byte[] input;
+         byte[] iv;
+         GetSampleData(out input, out iv);
+
+         var output = PokemonGoHashNative.Util.Hash(input, iv);
+         return output;
+      }
+
+      static byte[] TestManaged()
+      {
+         byte[] input;
+         byte[] iv;
+         GetSampleData(out input, out iv);
+
+         var output = PokemonGoHashSharp.Util.Hash(input, iv);
+         return output;
+      }
+
+      static void GetSampleData(out byte[] input, out byte[] iv)
+      {
+         input = new byte[5000];
+         for (var i = 0; i < input.Length; i++)
+         {
+            input[i] = (byte)i;
+         }
+         iv = new byte[32];
+         for (var i = 0; i < iv.Length; i++)
+         {
+            iv[i] = (byte)i;
+         }
+      }
+   }
+}
---
+++ PokemonGoHashSharp/PokemonGoHashNative/Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGoHashNative
+{
+    public class Util
+    {
+
+      [DllImport("encryptx64", CallingConvention = CallingConvention.Cdecl, EntryPoint = "encrypt")]
+      static extern int native_encrypt_x64(byte[] input, ulong inputSize, byte[] iv, ulong ivSize, byte[] output, ref ulong outputSize);
+
+      [DllImport("encrypt", CallingConvention = CallingConvention.Cdecl, EntryPoint = "encrypt")]
+      static extern int native_encrypt(byte[] input, uint inputSize, byte[] iv, uint ivSize, byte[] output, ref uint outputSize);
+
+
+      public static byte[] Hash(byte[] input, byte[] iv)
+      {
+         byte[] output = new byte[ushort.MaxValue];
+
+         int result;
+         int outputLength;
+
+         //32 bit
+         if (IntPtr.Size == 4)
+         {
+            var outL = (uint)output.Length;
+            result = native_encrypt(input, (uint)input.Length, iv, (uint)iv.Length, output, ref outL);
+            outputLength = (int)outL;
+         }
+         //64 bit
+         else if (IntPtr.Size == 8)
+         {
+            var outL = (ulong)output.Length;
+            result = native_encrypt_x64(input, (ulong)input.Length, iv, (ulong)iv.Length, output, ref outL);
+            outputLength = (int)outL;
+         }
+         else throw new ApplicationException("wtf this running on?");
+
+         if (result != 0)
+         {
+            throw new ApplicationException($"Failed with result code {result}");
+         }
+
+         var truncatedOutput = new byte[outputLength];
+         Buffer.BlockCopy(output, 0, truncatedOutput, 0, outputLength);
+         return truncatedOutput;
+      }
+
+   }
+}
